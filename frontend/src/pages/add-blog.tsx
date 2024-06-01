f611e0f72cfda56cf5cabfd735e01469997f0d96
Shristi Sharan <shristisharan05@gmail.com>
2024-06-01 12:48:35 +0530

    fix-#77: Refactor Form validation using react-hook-form and zod

--- frontend/src/pages/add-blog.tsx
+++ frontend/src/pages/add-blog.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,80 +12,103 @@ import userState from '@/utils/user-state';
 import axiosInstance from '@/helpers/axios-instance';
 import { AxiosError, isAxiosError } from 'axios';
 
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 type FormData = {
   title: string;
   authorName: string;
-  imageLink: string;
+  image: string;
   categories: string[];
   description: string;
   isFeaturedPost: boolean;
 };
 function AddBlog() {
   const [selectedImage, setSelectedImage] = useState<string>('');
+  const navigate = useNavigate();
 
   const handleImageSelect = (imageUrl: string) => {
     setSelectedImage(imageUrl);
   };
-
   const [modal, setmodal] = useState(false);
   const [formData, setFormData] = useState<FormData>({
     title: '',
     authorName: '',
-    imageLink: '',
+    image: '',
     categories: [],
     description: '',
     isFeaturedPost: false,
   });
 
+  const addBlogSchema = z.object({
+    title: z.string().min(3, 'Thats it? Give atleast 3 words.'),
+    description: z.string().min(10, 'Go on, spill the beans'),
+    authorName: z.string().min(2, "C'mon your name cannot be less than 3 characters" ),
+    image: z.union([
+      z.string().url({ message: 'Image URL must be a valid URL' }),
+      z.object({ type: z.literal('image/*').optional() }),]),
+    // z.string().url('You Forgot the Image!'),
+    // categories: z.array(z.string()).min(1, 'Select at least 1 category').max(3, 'Select up to 3 categories'),
+
+  });
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(addBlogSchema)
+  });
+
   //checks the length of the categories array and if the category is already selected
   const isValidCategory = (category: string): boolean => {
-    return formData.categories.length >= 3 && !formData.categories.includes(category);
-  };
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    return formData.categories.length >=3 || !formData.categories.includes(category);
   };
 
   const handleCategoryClick = (category: string) => {
-    if (isValidCategory(category)) return;
+    console.log("Current categories:", formData.categories);
+    if (!isValidCategory(category)) return;
 
     if (formData.categories.includes(category)) {
       setFormData({
         ...formData,
-        categories: formData.categories.filter((cat) => cat !== category),
+        categories:formData.categories.concat(category),
       });
     } else {
       setFormData({
         ...formData,
         categories: [...formData.categories, category],
       });
+      console.log("Updated formData:", formData.categories);
     }
   };
+  useEffect(() => {
+    console.log("Updated categories in useEffect:", formData.categories);
+  }, []);
 
   const handleselector = () => {
     setFormData({
       ...formData,
-      imageLink: selectedImage,
+      image: selectedImage,
     });
     setmodal(false);
   };
   const handleCheckboxChange = () => {
     setFormData({ ...formData, isFeaturedPost: !formData.isFeaturedPost });
   };
-  const validateFormData = () => {
+  
+  const validateFormData = (data: FormData) => {
+    console.log(data.categories.length)
     if (
-      !formData.title ||
-      !formData.authorName ||
-      !formData.imageLink ||
-      !formData.description ||
-      formData.categories.length === 0
+      !data.title ||
+      !data.authorName ||
+      !data.image ||
+      !data.description ||
+      data.categories.length < 1 ||
+      data.categories.length > 3
     ) {
       toast.error('All fields must be filled out.');
       return false;
     }
     const imageLinkRegex = /\.(jpg|jpeg|png|webp)$/i;
-    if (!imageLinkRegex.test(formData.imageLink)) {
+    if (!imageLinkRegex.test(data.image)) {
       toast.error('Image URL must end with .jpg, .jpeg, .webp or .png');
       return false;
     }
@@ -95,12 +119,12 @@ function AddBlog() {
 
     return true;
   };
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (validateFormData()) {
+
+  const onSubmit = async (data: FormData) => {
+    if (validateFormData(data)) {
+      console.log("Submitted data:", data)
       try {
         const postPromise = axiosInstance.post('/api/posts/', formData);
-
         toast.promise(postPromise, {
           pending: 'Creating blog post...',
           success: {
@@ -108,7 +132,7 @@ function AddBlog() {
               setFormData({
                 title: '',
                 authorName: '',
-                imageLink: '',
+                image: '',
                 categories: [],
                 description: '',
                 isFeaturedPost: false,
@@ -142,7 +166,6 @@ function AddBlog() {
       }
     }
   };
-  const navigate = useNavigate();
   const [isDarkMode, setIsDarkMode] = useState<boolean | null>(null);
   useEffect(() => {
     const storedTheme = localStorage.getItem('theme');
@@ -152,7 +175,6 @@ function AddBlog() {
   function Asterisk() {
     return <span className="dark:text-dark-tertiary">*</span>;
   }
-
   return (
     <div className="flex-grow cursor-default bg-slate-50 px-6 py-8 dark:bg-dark">
       <div className="mb-4 flex justify-center">
@@ -171,7 +193,7 @@ function AddBlog() {
         </div>
       </div>
       <div className="flex justify-center">
-        <form onSubmit={handleSubmit} className="sm:w-5/6 lg:w-2/3">
+        <form onSubmit={handleSubmit(onSubmit, (e)=> console.log(e))} className="sm:w-5/6 lg:w-2/3">
           <div className="mb-2 flex items-center">
             <label className="flex items-center">
               <span className="px-2 text-base font-medium text-light-secondary dark:text-dark-secondary">
@@ -189,31 +211,36 @@ function AddBlog() {
 
           <div className="mb-2">
             <div className="px-2 py-1 font-medium text-light-secondary dark:text-dark-secondary">
-              Blog title <Asterisk />
-            </div>
+            <label htmlFor="title" title="must be longer than 3">
+              Blog Title <Asterisk />
+            </label>
             <input
+              id="text"
               type="text"
-              name="title"
               placeholder="Travel Bucket List for this Year"
               autoComplete="off"
               className="w-full rounded-lg bg-slate-200 p-3 placeholder:text-sm placeholder:text-light-tertiary dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
-              value={formData.title}
-              onChange={handleInputChange}
-            />
+              {...register("title")}
+              />
+              {errors.title?.message && <p className="text-red-500">{errors.title?.message}</p>}
           </div>
-
+          </div>
+          
           <div className="mb-1">
             <div className="px-2 py-1 font-medium text-light-secondary dark:text-dark-secondary">
+            <label htmlFor="concent" title="Description should be clear and longer than 10 characters">
               Blog content <Asterisk />
+            </label>
             </div>
             <textarea
-              name="description"
               placeholder="Start writing here&hellip;"
               rows={5}
               className="w-full rounded-lg bg-slate-200 p-3 placeholder:text-sm placeholder:text-light-tertiary dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
-              value={formData.description}
-              onChange={handleInputChange}
+              {...register('description')}
             />
+            {errors.description?.message && (
+              <p className="text-red-500">{errors.description?.message}</p>
+              )}
           </div>
 
           <div className="mb-2">
@@ -222,14 +249,14 @@ function AddBlog() {
             </div>
             <input
               type="text"
-              name="authorName"
               placeholder="Shree Sharma"
               className="w-full rounded-lg bg-slate-200 p-3 placeholder:text-sm placeholder:text-light-tertiary dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
-              value={formData.authorName}
-              onChange={handleInputChange}
+              {...register('authorName')}
             />
+            {errors?.authorName && <p className="text-red-500">{errors.authorName.message}</p>}
           </div>
 
+        <div className="mb-2">
           <div className="px-2 py-1 font-medium text-light-secondary dark:text-dark-secondary">
             Blog cover image
             <span className="text-xs tracking-wide text-dark-tertiary">
@@ -241,13 +268,13 @@ function AddBlog() {
             <input
               type="url"
               id="imagelink"
-              name="imageLink"
               placeholder="https://&hellip;"
               autoComplete="off"
+              accept="image/*"
               className="w-3/4 rounded-lg bg-slate-200 p-3 placeholder:text-sm placeholder:text-light-tertiary dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary lg:w-10/12"
-              value={formData.imageLink}
-              onChange={handleInputChange}
-            />
+              value={formData.image}
+              {...register('image')}
+            />            
             <button
               name="openModal"
               type="button"
@@ -258,6 +285,8 @@ function AddBlog() {
             >
               Pick image
             </button>
+            {errors.image && <p className="text-red-500">{errors.image.message}</p>}
+          </div>
           </div>
           <div className="mb-4 flex flex-col">
             <label className="px-2 pb-1 font-medium text-light-secondary dark:text-dark-secondary sm:mr-4 sm:w-fit">
@@ -267,18 +296,18 @@ function AddBlog() {
               </span>
               <Asterisk />
             </label>
-            <div className="flex flex-wrap gap-3 rounded-lg p-2 dark:bg-dark-card dark:p-3">
+            <div className="flex flex-wrap gap-3 rounded-lg p-2 dark:bg-dark-card dark:p-3">      
               {categories.map((category, index) => (
                 <span key={`${category}-${index}`} onClick={() => handleCategoryClick(category)}>
                   <CategoryPill
                     category={category}
                     selected={formData.categories.includes(category)}
-                    disabled={isValidCategory(category)}
+                    disabled={!isValidCategory(category)}
                   />
                 </span>
               ))}
             </div>
-          </div>
+            </div>
 
           <button
             name="post-blog"
@@ -301,3 +330,10 @@ function AddBlog() {
 }
 
 export default AddBlog;
+
+
+
+
+
+
+
